98c44f3a6dad518d1b1e6cb042de95c3b13f3310
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-19 09:58:05 -0700

    terraform

--- variables.tf
+++ variables.tf
@@ -5,7 +5,11 @@ variable "ami_id" {
 }
 
 variable "instance_type" {
-    
+    type        = string
+    validation {
+        condition     = contains(["t3.micro", "t3.medium", "t3.small"], var.instance_type)
+        error_message = "instance_type can only be one of t3.micro, t3.medium, t3.small"
+    } 
 }
 
 variable "security_group" {
